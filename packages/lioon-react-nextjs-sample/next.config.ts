b9db631fc6ff5a51d3fb25bc294e76f797104212
horita-yuya <horitayuya@gmail.com>
2025-05-11 19:44:46 +0900

    next

--- packages/lioon-react-nextjs-sample/next.config.ts
+++ packages/lioon-react-nextjs-sample/next.config.ts
@@ -1,7 +1,17 @@
+import lioonNextPlugin from "@lioon/react/next";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig: NextConfig = lioonNextPlugin({
+  outputDir: "src/i18n",
+  supportedLocales: ["en", "ja", "ko", "zh", "es"],
+  // Optional: Custom translation function
+  // staticTranslate: async (templates) => {
+  //   return templates.map((template) => {
+  //     return [template, { en: template, ja: template, ko: template, zh: template, es: template }]
+  //   })
+  // },
+})({
+  /* Other Next.js config options here */
+});
 
 export default nextConfig;
