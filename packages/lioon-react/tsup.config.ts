a15ff09a5927e5670dc36fc6f481e4e4c7fdda55
horita-yuya <horitayuya@gmail.com>
2025-05-21 22:54:19 +0900

    bump

---
+++ packages/lioon-react/tsup.config.ts
@@ -0,0 +1,13 @@
+// tsup.config.ts
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: [
+    'src/index.ts',
+    'src/vite/index.ts',
+    'src/next/index.ts',
+  ],
+  format: ['esm'],
+  dts: true,
+  banner: { js: '"use client";' }, // ← これだけで OK
+})
